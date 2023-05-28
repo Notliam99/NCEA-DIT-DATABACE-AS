47cbf6f558c9094afed9c8d46794bba61fec3727
Liam Tietjens <l.tietjens@hotmail.com>
2023-05-29 11:07:24 +1200

    fixed major speeling mistake. :) broke hole ci pipe line.
    	modified:   .github/workflows/ci.yml

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ import sqlite3
 
 with sqlite3.connect("employee.sqlite3") as connection:
     cursor = connection.cursor()
-    cursor.execute("create table cool (wow);")
-    connection.commit()
+    cursor.
