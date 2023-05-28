38f5433675c9adea379875743ee5e4826a953d5c
Liam Tietjens <l.tietjens@hotmail.com>
2023-05-28 18:50:08 +1200

    added layout and some perlimary data very cool added one employee 	modified:   employee.sqlite3

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import sqlite3
 
 with sqlite3.connect("employee.sqlite3") as connection:
     cursor = connection.cursor()
-    cursor.execute(";")
+    cursor.execute("create table cool (wow);")
+    connection.commit()
