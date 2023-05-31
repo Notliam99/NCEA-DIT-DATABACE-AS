533d984ebc536a7fb60a7ce243441424f5dcdb19
Liam Tietjens <l.tietjens@hotmail.com>
2023-05-31 14:39:40 +1200

    adding comments becuse i forgot such a baby programer mistake i blame the lack of comments on idk i goofed 	modified:   main.py 	modified:   tests/test_place_holder.py

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 import sqlite3
 
 with sqlite3.connect("employee.sqlite3") as connection:
+    """
+        connects and runs a qureie the resault
+        of witch is saved to the var resault.
+    """
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM employees_table")
-    resault = cursor.fetchall()
+    resault = cursor.fetchall()  # the resault where the reault is saved.
 
 for person in resault:
+    """lists through the pepole in the table"""
     for item in person:
+        """lists through the items of info mation about each person."""
         print(item)
